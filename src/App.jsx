1d82a10457bc8f1e8da156e5510cb4f67243639e
jayantroy <141987105+reconroy@users.noreply.github.com>
2025-04-22 20:16:32 +0530

    codespace ui starts from here ♥ !

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className='text-3xl font-bold underline'>This is CodeSpace</p>
+    </>
+  )
+}
+
+export default App
