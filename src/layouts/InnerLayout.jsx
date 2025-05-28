671dc7622807cc8a0673e78782a22444c3c30eeb
jayantroy <141987105+reconroy@users.noreply.github.com>
2025-05-28 22:14:22 +0530

    Update InnerLayout.jsx

--- src/layouts/InnerLayout.jsx
+++ src/layouts/InnerLayout.jsx
@@ -13,10 +13,7 @@ const InnerLayout = () => {
   return (
     <div className="min-h-screen bg-gray-900 flex relative overflow-hidden">
       {/* Background effects */}
-      <div className="absolute inset-0 overflow-hidden pointer-events-none">
-        <div className="absolute -inset-[10%] bg-gradient-to-tr from-green-500/5 via-green-300/10 to-transparent rotate-12 transform-gpu blur-3xl"></div>
-        <div className="absolute -inset-[5%] top-[30%] bg-gradient-to-bl from-green-500/5 via-green-400/5 to-transparent -rotate-12 transform-gpu blur-2xl"></div>
-      </div>
+  
 
       {/* Sidebar */}
       <PrimaryPanel handleLogout={handleLogout} />
